fc1130d36fb734f8ed215d734bb26745fe396fba
Chirag Chauhan <abhishekchauhan01233>
2020-05-30 17:12:56 +0530

    1

--- stest/settings.py
+++ stest/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'Desktop/exam/main/static'),
+    os.path.join(BASE_DIR,'main/static'),
 ]
 
 MEDIA_URL = '/media/'
