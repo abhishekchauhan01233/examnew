3270516f597fb323031e1ed303d5291053b18ae6
Chirag Chauhan <abhishekchauhan01233>
2020-06-02 11:43:40 +0530

    564

--- main/views.py
+++ main/views.py
@@ -194,8 +194,8 @@ def take_test(request):
     timenow_ = datetime.strftime(datetime.now(),"%H:%M:%S")
     t = [timenow_,'05:30:00']
     for i in t:
-	timeparts = [int(s) for s in i.split(':')]
-	totalsecs += (timeparts[0]*60 + timeparts[1])*60 + timeparts[2]
+	    timeparts = [int(s) for s in i.split(':')]
+	    totalsecs += (timeparts[0]*60 + timeparts[1])*60 + timeparts[2]
     totalsecs, sec = divmod(totalsecs, 60)
     hr, minute = divmod(totalsecs, 60)
     timenow = "%02d:%02d:%02d" % (hr,minute,sec)
